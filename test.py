eeb34765cb0d91761ac9c9515c7661a2be61d5d2
John Tsantilis <i.tsantilis@yahoo.com>
2018-11-27 17:30:08 +0200

    refactor(plots): code refactoring

--- test.py
+++ test.py
@@ -11,40 +11,65 @@ data_req = pd.read_csv(
     names=['Initial_data', 'x', 'Burr', 'Dagum', 'Pearson_5_3P'],
     sep=",")
 
-cdf_data = data_req.x
+initial_data = data_req.Initial_data
+initial_data.dropna()
+x = data_req.x
 burr = data_req.Burr
 dagum = data_req.Dagum
 pearson_5_3P = data_req.Pearson_5_3P
-initial_data = data_req.Initial_data
-initial_data.dropna()
 
 # Create traces
-trace0 = go.Scatter(
-    x = burr,
-    mode = 'lines',
-    name = 'burr'
-)
-trace1 = go.Scatter(
-    x = dagum,
-    mode = 'lines',
-    name = 'dagum'
-)
-trace2 = go.Scatter(
-    x = pearson_5_3P,
-    mode = 'lines',
-    name = 'pearson_5_3P'
-)
-
-data = [go.Histogram(
+trace0 = go.Histogram(
     histfunc="count",
     histnorm="probability density",
     x=initial_data,
     xbins=dict(
         start='1.9',
         end='10',
-        size='1.1'),
+        size='1.0125'),
     autobinx=False,
-    cumulative=dict(enabled=True))]
+    marker=dict(
+        color='rgb(158,202,225)',
+        line=dict(
+            color='rgba(17, 157, 255, 0.5)',
+            width=1.5,
+        )
+    ),
+    opacity=0.1,
+    cumulative=dict(enabled=True)
+)
+
+trace1 = go.Scatter(
+    x=x,
+    y=burr,
+    mode='lines',
+    name='burr'
+)
+
+trace2 = go.Scatter(
+    x=x,
+    y=dagum,
+    mode='lines',
+    name='dagum'
+)
+
+trace3 = go.Scatter(
+    x=x,
+    y=pearson_5_3P,
+    mode='lines',
+    name='pearson_5_3P'
+)
+
+# data = [go.Histogram(
+#     histfunc="count",
+#     histnorm="probability density",
+#     x=initial_data,
+#     xbins=dict(
+#         start='1',
+#         end='10',
+#         size='1.2'),
+#     autobinx=False,
+#     cumulative=dict(enabled=True))]
 
 # data = [go.Histogram(
 #     histfunc="count",
@@ -54,16 +79,17 @@ data = [go.Histogram(
 #     autobinx=False,
 #     cumulative=dict(enabled=True))]
 
-# data = [trace0, trace1, trace2]
-
 # data = [go.Histogram(
 #     histfunc="count",
 #     histnorm="probability density",
-#     x=cdf_data,
-#     nbinsx=10,
+#     x=initial_data,
+#     nbinsx=9,
 #     cumulative=dict(enabled=True)),
 #     trace0,
 #     trace1,
 #     trace2]
 
+data = [trace0, trace1, trace2, trace3]
+
 plotly.offline.plot(data, filename='cumulativeHistogram.html')
+#plotly.offline.plot(data, filename='cumulativeHistogram.html', image="svg")
